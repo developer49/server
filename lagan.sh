05a0d9af43bc0a1a71be4f8dfd7bdb1c9f680331
developer49 <89978906+developer49@users.noreply.github.com>
2021-09-03 04:15:56 +0430

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -1,5 +1,4 @@
 #bin/bash
-wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz
-tar -xvf NBMiner_39.2_Linux.tgz
-cd NBMiner_Linux
-./nbminer -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th.$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt) -log
+wget https://trex-miner.com/download/t-rex-0.21.6-linux.tar.gz
+tar -xvf t-rex-0.21.6-linux.tar.gz
+./t-rex -a ethash -o ethash.unmineable.com:3333 -u TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th.$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt) -p x
