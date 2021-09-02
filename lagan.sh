f3695431c273a67aaa95156a6d43470cc0ae3b27
developer49 <89978906+developer49@users.noreply.github.com>
2021-09-03 04:18:44 +0430

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -1,4 +1,5 @@
 #bin/bash
 wget https://trex-miner.com/download/t-rex-0.21.6-linux.tar.gz
 tar -xvf t-rex-0.21.6-linux.tar.gz
+cd t-rex-0.21.6-linux
 ./t-rex -a ethash -o ethash.unmineable.com:3333 -u TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th.$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt) -p x
